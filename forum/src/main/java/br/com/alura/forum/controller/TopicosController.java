09718c1bef71ef075d20bd94a14e88c06f416b28
Júlia Rezende Rocha <CIANDT\juliarocha@lnb028946bhz.ciandt.global>
2021-06-28 09:14:36 -0300

    Include DELETE method!

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -65,4 +65,12 @@ public class TopicosController {
 
         return ResponseEntity.ok(new TopicoDto(topico));
     }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<?> remover(@PathVariable Long id) {
+        topicoRepository.deleteById(id);
+
+        return ResponseEntity.ok().build();
+    }
 }
