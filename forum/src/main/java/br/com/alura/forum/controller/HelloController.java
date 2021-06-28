bf10592db4dd0a98112191eb91b162194edff5e4
Júlia Rezende Rocha <CIANDT\juliarocha@lnb028946bhz.ciandt.global>
2021-06-28 06:55:44 -0300

    Inlude endpoint /topicos

--- forum/src/main/java/br/com/alura/forum/controller/HelloController.java
+++ forum/src/main/java/br/com/alura/forum/controller/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorld {
+public class HelloController {
     @RequestMapping("/")
     public String hello(){
         return  "Hello World!";
