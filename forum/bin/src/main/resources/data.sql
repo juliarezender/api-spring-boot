a7911e9a3742c6ee001ccf308c9f6a09e180913e
Júlia Rezende Rocha <CIANDT\juliarocha@lnb028946bhz.ciandt.global>
2021-07-08 11:46:48 -0300

    Authotization based on roles

--- forum/bin/src/main/resources/data.sql
+++ forum/bin/src/main/resources/data.sql
@@ -1,11 +1,4 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$Al1KD2HWJz4otM5ff7nXa.aZipV/5kq5y/oqsMVKYICs6916vNpue');
-INSERT INTO USUARIO(nome, email, senha) VALUES('Moderador', 'moderador@email.com', '$2a$10$Al1KD2HWJz4otM5ff7nXa.aZipV/5kq5y/oqsMVKYICs6916vNpue');
-
-INSERT INTO PERFIL(id, nome) VALUES(1, 'ROLE_ALUNO');
-INSERT INTO PERFIL(id, nome) VALUES(2, 'ROLE_MODERADOR');
-
-INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES(1, 1);
-INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES(2, 2);
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
